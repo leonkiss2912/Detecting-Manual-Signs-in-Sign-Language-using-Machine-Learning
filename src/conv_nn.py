e708f1bcdc1f8986426c7d3cf6174a12bd4af903
Leon Kiss (Home PC) <leonkiss92@gmail.com>
2025-06-12 01:31:41 +0200

    Added Augmentation Dataset to populate underrepresented classes.

--- src/conv_nn.py
+++ src/conv_nn.py
@@ -8,14 +8,14 @@ class ConvNN(nn.Module):
         self.conv1 = nn.Conv2d(1, 16, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(16, 32, 5)
-        self.adaptive_pool = nn.AdaptiveAvgPool2d((10, 10))
+        self.adaptive_pool = nn.AdaptiveAvgPool2d((5, 5))
         '''
             After conv1:        W - F + 1 = 128 - 5 + 1 = 124   (=> 16 Channels of 124x124)
             After max pooling:  W/2 = 124/2 = 62                (=> 16 Channels of 62x62)
             After conv2:        W - F + 1 = 62 - 5 + 1 = 58     (=> 32 Channels of 58x58)
             After adaptive pooling: 32*5*5                      (=> 32 Channels of 5x5)
         '''
-        self.fc1 = nn.Linear(32*10*10, 120)
+        self.fc1 = nn.Linear(32*5*5, 120)
         self.fc2 = nn.Linear(120, 84)
         self.fc3 = nn.Linear(84, num_classes)
 
@@ -33,7 +33,7 @@ class ConvNN(nn.Module):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
         x = self.adaptive_pool(x)
-        x = x.view(-1, 32*10*10)
+        x = x.view(-1, 32*5*5)
         x = F.relu(self.fc1(x))
         x = F.relu(self.fc2(x))
         x = self.fc3(x)
